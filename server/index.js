651d9e550ab2bf9e5d44b79d9ffdef9672642729
visak14 <121089328+visak14@users.noreply.github.com>
2024-07-13 13:56:38 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cors(
   {
-  origin: ["https://courses-app-frontend-lgfb5tkip-visak14s-projects.vercel.app"],
+  origin: ["https://courses-app-frontend.vercel.app"],
   methods: ["POST", "GET", "PUT", "PATCH" ],
   credentials: true
   }
