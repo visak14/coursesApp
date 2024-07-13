86918fab16662c4a84f45e78ae98546f42edd8e4
visak14 <121089328+visak14@users.noreply.github.com>
2024-07-13 13:24:33 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,9 +17,11 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cors(
+  {
   origin: ["https://courses-app-seven.vercel.app"],
   methods: ["POST", "GET" ],
   credentials: true
+  }
 ))
 app.use('/admin', adminRouter)
 app.use('/user',userRouter)
