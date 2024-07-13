a68ba42c5e0ebe5588ef7cadc2e00d8f1be02d6a
visak14 <121089328+visak14@users.noreply.github.com>
2024-07-13 14:14:34 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,21 +1,19 @@
-const express = require('express')
+const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose')
-
-
-
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
 dotenv.config();
 
-
 const port = process.env.PORT;
-const uri = process.env.URI
-const adminRouter = require('./routes/admin')
-const userRouter = require('./routes/user')
+const uri = process.env.URI;
+const adminRouter = require('./routes/admin');
+const userRouter = require('./routes/user');
 
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const allowedOrigins = ['https://courses-app-frontend.vercel.app'];
 
 const corsOptions = {
@@ -30,14 +28,17 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.use('/admin', adminRouter)
-app.use('/user',userRouter)
 
+app.use('/admin', adminRouter);
+app.use('/user', userRouter);
 
-
-
-
-
-mongoose.connect(uri, { dbName: "course"})
-
-app.listen(port, ()=> {console.log(`server running on ${port}`)})
+mongoose.connect(uri, { dbName: 'course' })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch(err => {
+    console.error('Failed to connect to MongoDB', err);
+  });
