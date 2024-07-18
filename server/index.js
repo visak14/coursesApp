ab3661e671f8b40dee7c09901d710be8d9581c23
visak14 <121089328+visak14@users.noreply.github.com>
2024-07-18 23:08:10 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,39 +1,29 @@
-const express = require('express');
+const express = require('express')
 const cors = require('cors');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const mongoose = require('mongoose')
 
+
+const dotenv = require('dotenv');
 dotenv.config();
 
-const port = process.env.PORT || 5000; // Set a default port in case PORT is not defined
-const uri = process.env.URI;
-const adminRouter = require('./routes/admin');
-const userRouter = require('./routes/user');
 
-const app = express();
+const port = process.env.PORT;
+const uri = process.env.URI
+const adminRouter = require('./routes/admin')
+const userRouter = require('./routes/user')
 
-// CORS configuration
-const corsOptions = {
-  origin: 'https://courses-app-frontend.vercel.app', // Only allow requests from this origin
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
-  optionsSuccessStatus: 204
-};
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
+app.use('/admin', adminRouter)
+app.use('/user',userRouter)
 
-app.use(cors(corsOptions));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-app.use('/admin', adminRouter);
-app.use('/user', userRouter);
 
-mongoose.connect(uri, { dbName: 'course' })
-  .then(() => {
-    console.log('Connected to MongoDB');
-    app.listen(port, () => {
-      console.log(`Server running on port ${port}`);
-    });
-  })
-  .catch(err => {
-    console.error('Database connection error:', err);
-  });
+
+
+
+mongoose.connect(uri, { dbName: "course"})
+
+app.listen(port, ()=> {console.log(`server running on ${port}`)})
